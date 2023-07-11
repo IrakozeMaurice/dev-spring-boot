c4cc0dc923697be0af054a0251b5c9f05460f347
Irakoze Maurice <iramaurimade@gmail.com>
2023-07-11 14:01:13 +0200

    dependency injection: bean lifecycle methods

---
+++ 02-spring-boot-spring-core/08-lifecyle methods/src/main/java/com/irakozemaurice/springcoredemo/BaseballCoach.java
@@ -0,0 +1,16 @@
+package com.irakozemaurice.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+	
+	public BaseballCoach() {
+		System.out.println("In constructor: " + getClass().getSimpleName());
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "spend 30 minutes in batting Practice";
+	}
+}
---
+++ 02-spring-boot-spring-core/08-lifecyle methods/src/main/java/com/irakozemaurice/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.irakozemaurice.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	private Coach coach;
+
+	// define a constructor for dependency injection
+	@Autowired
+	public DemoController(@Qualifier("cricketCoach") Coach coach) {
+		this.coach = coach;
+	}
+
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return coach.getDailyWorkout();
+	}	
+}
